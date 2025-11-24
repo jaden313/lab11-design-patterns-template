d8ff2b8abede35b29bd49c1b61cd3eec5a0910b5
Jaden Ziga <179839509+jaden313@users.noreply.github.com>
2025-11-24 03:26:30 +0000

    Task 5-6 completed

---
+++ presidio-anonymizer/tests/operators/test_initial.py
@@ -0,0 +1,20 @@
+from presidio_anonymizer.operators import Initial
+
+
+def test_correct_name():
+    assert Initial().operator_name() == "initial"
+
+
+import pytest
+
+@pytest.mark.parametrize(
+    "input_text, initials",
+    [
+        ("John Smith", "J. S."),
+        ("     Eastern    Michigan   University ", "E. M. U."),
+    ],
+)
+def test_given_value_for_initial(input_text, initials):
+    text = Initial().operate(input_text)
+    assert text == initials
+
